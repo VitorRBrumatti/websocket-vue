ea378531627c06a5c733ccc8cbc57750e88ee469
VitorRBrumatti <vitorrufino084@gmail.com>
2024-07-17 16:08:11 -0300

    Create: create-branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+      <div v-for="message in messages" :key="message">{{ message }}</div>
+      <input v-model="message" @keyup.enter="sendMessage" placeholder="Type your message" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          message: '',
+          messages: []
+      };
+  },
+  mounted() {
+      window.Echo.channel('chat')
+          .listen('MessageSent', (e) => {
+              this.messages.push(e.message);
+          });
+  },
+  methods: {
+      sendMessage() {
+          axios.post('/send-message', {
+              message: this.message
+          }).then(response => {
+              this.message = '';
+          });
+      }
+  }
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+  .background {
+    height: 100%;
+    width: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>
+            Login Page!
+        </h1>
+        <button>Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login'
+    };
+</script>
+
+<style  scoped>
+
+</style>
